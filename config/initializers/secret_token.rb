558a7adde5d28665dc1a2d8a2529dcb18e78e9ae
josue <jfigueroagama@redesvozdatos.com>
2012-07-01 15:18:45 -0500

    Initial academic control commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcademicControl::Application.config.secret_token = 'ec7519c4dce91f38ce03860be0eb6c7fd536942425e51fd66ec80a9d8b5b80596d3a142973381957676e2bbe7a13e86f02e54db27d31829744105829b1e2c3f8'
